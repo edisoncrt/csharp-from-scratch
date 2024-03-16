651c2e9b9493c40c763df2dd97c8582f8b965be5
Edison Rodríguez <edisonrodriguez106998@correo.itm.edu.co>
2024-03-16 16:22:08 -0500

    Logical Operators and Conditionals

---
+++ conditionals_and_operators/Program.cs
@@ -0,0 +1,23 @@
+﻿/*conditionales*/
+int num;
+Console.WriteLine("Ingrese un numero:");
+/*conditionales/*
+string nameImput = Console.ReadLine();
+num = int.Parse(nameImput);
+if (num > 0) Console.WriteLine("El numero es positivo");
+else if (num < 0) Console.WriteLine("El numero es negativo");
+else Console.WriteLine("cero");
+
+/*conditionales*/
+
+/*Operators*/
+
+Console.WriteLine("Ingrese el primer numero");
+double a= double.Parse(Console.ReadLine());
+Console.WriteLine("Ingrese el segundo numero");
+double b= double.Parse(Console.ReadLine());
+ String Indeterminado="indeterminado";
+
+Console.WriteLine($"suma: {a+b}, resta: {a-b}, multiplicacion: {a*b}, division: {(b != 0 ? (a / b).ToString() : Indeterminado)}");
+
+
